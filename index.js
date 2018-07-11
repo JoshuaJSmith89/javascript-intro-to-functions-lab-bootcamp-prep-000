e43942d1902a0fcc85ec4e60417c07151a662d2d
Joshua Smith <jjsmith260@hotmail.com>
2018-07-11 06:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ function logShout(string) {
 
 function sayHiToGrandma(string)	{
 	if(string.toLowerCase() === string)	{
-		return "I can't hear you!";
+		return "I can't hear you";
+	}
+	
+	if(string.toUpperCase() === string)	{
+		return "YES INDEED!";
+	}
+	
+	if(string.toLowerCase() === "I love you, Grandma.")	{
+		return "I love you, too.";
 	}
 }
